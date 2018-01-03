e061e944e62aebf43a8c0d6a2b8e067d37ec2d54
jcgato93 <npgcamilo@gmail.com>
2018-01-02 23:06:56 -0500

    init commit

---
+++ Nexos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nexos.MvcApplication" Language="C#" %>
---
+++ Nexos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nexos.Startup))]
+namespace Nexos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
